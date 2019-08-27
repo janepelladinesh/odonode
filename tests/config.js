33bd4f091f21cff54ccd6d89b953582d511e4e66
Dinesh <janepelladinesh97@gmail.com>
2019-08-27 15:50:21 +0530

    Add files via upload

---
+++ tests/config.js
@@ -0,0 +1,10 @@
+
+var mongoHost = '172.30.169.192'
+var mongoPort = 27017
+console.log("mongohost:port ="+mongoHost+":"+mongoPort);
+module.exports = {
+	'secret': 'saregama',
+	//'database':'mongodb://localhost/demo'
+	//'database': 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_SERVICE_HOST+':27017/'+process.env.MONGODB_DATABASE
+	'database': 'mongodb://'+mongouser+':'+password+'@'+mongoHost+':'+mongoPort+'/'+userdb
+};
